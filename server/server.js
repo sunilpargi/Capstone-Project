6b07a10ec55ba6f8e3743d4c97014e3ad977333b
Sunil Pargi <110519400+sunilpargi123@users.noreply.github.com>
2024-05-26 22:51:46 +0530

    Update server to connect to MongoDB Atlas and

--- server/server.js
+++ server/server.js
@@ -12,14 +12,15 @@ app.get('/', (req, res) => {
   res.send('ShopPlusPlus API');
 });
 
-mongoose.connect('mongodb://localhost:27017/shopplusplus', {
+mongoose.connect('mongodb+srv://sunilpargi121:ezSCk51xgQTJQFda@cluster0.kzaxdlb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB Atlas');
 }).catch((error) => {
-  console.log('MongoDB connection error:', error);
+  console.log('MongoDB Atlas connection error:', error);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
